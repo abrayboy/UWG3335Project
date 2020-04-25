a2f3b2b24b4eeedca9ebaa0ec4cc3600d486035f
ceesvr <allen.justin78@gmail.com>
2020-04-25 15:40:45 -0400

    Added Backend

---
+++ Team_Project/api/Apparel.ashx
@@ -0,0 +1,1 @@
+﻿<%@ WebHandler Language="C#" CodeBehind="Apparel.ashx.cs" Class="Team_Project.api.Apparel" %>
---
+++ Team_Project/api/model/Cart.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Team_Project.api.model
+{
+    public class Cart
+    {
+
+        public IList<ApparelItem> ApparelItems { get; }
+
+        public float Total
+        {
+            get
+            {
+                return ApparelItems.Sum(item => item.Price);
+            }
+        }
+
+        public Cart()
+        {
+            this.ApparelItems = new List<ApparelItem>();
+        }
+
+        public void Add(ApparelItem apparelItem)
+        {
+            this.ApparelItems.Add(apparelItem);
+        }
+
+        public void Remove(ApparelItem apparelItem)
+        {
+            this.ApparelItems.Remove(apparelItem);
+        }
+
+        public ApparelItem Get(int index)
+        {
+            return this.ApparelItems[index];
+        }
+
+        public bool Contains(ApparelItem item)
+        {
+            return this.ApparelItems.Contains(item);
+        }
+    }
+}
